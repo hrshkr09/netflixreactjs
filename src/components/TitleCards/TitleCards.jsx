995b3310d09197077f1cc00fd87ce926e31dd8b5
Harsh Kumar <harshsingh1393@gmail.com>
2024-09-15 17:49:56 +0530

    created the folder structure of the project

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './TitleCards.css'
+const TitleCards = () => {
+  return (
+    <div className='titlecards'>
+      Titlecards
+    </div>
+  );
+}
+
+export default TitleCards;
