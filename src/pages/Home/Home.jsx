b00c1f5b8dd644443363942aa6333ae811aed840
Harsh Kumar <harshsingh1393@gmail.com>
2024-09-15 18:42:01 +0530

    completed the navbar design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import "./Home.css"
-const Home =()=>{
-    return(
-        <div className='home'>Home</div>
-    )
-}
-
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  );
+};
 
 export default Home;
