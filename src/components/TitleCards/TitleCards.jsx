131b4329f22f82904a81bbf3678dbf3a86c19732
Harsh Kumar <harshsingh1393@gmail.com>
2024-09-16 12:54:33 +0530

    completed the designing of home page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import './TitleCards.css'
-const TitleCards = () => {
+import React, { useEffect, useRef } from 'react';
+import './TitleCards.css';
+import cards_data from '../../assets/cards/Cards_data';
+
+const TitleCards = ({title,category}) => {
+
+  const cardsRef=useRef();
+
+  const handleWheel=(event)=>{
+    event.preventDefault();
+    cardsRef.current.scrollLeft +=event.deltaY;
+  }
+  useEffect(()=>{
+    cardsRef.current.addEventListener('wheel',handleWheel)
+  },[])
   return (
-    <div className='titlecards'>
-      Titlecards
+    <div className='title_cards'>
+      <h2>{title?title: "Popular on Netflix"}</h2>
+      <div className="card_list" ref={cardsRef}>
+        {cards_data.map((card, index)=>{
+          return <div className="card" key={index}>
+              <img src={card.image} alt=""  />
+              <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   );
 }
