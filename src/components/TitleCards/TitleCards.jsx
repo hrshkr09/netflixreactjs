5451026faae8978c0ae550f325595a794c0b1461
Harsh Kumar <harshsingh1393@gmail.com>
2024-09-16 23:43:02 +0530

    completed playeer component

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,26 +1,41 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data';
 
 const TitleCards = ({title,category}) => {
 
   const cardsRef=useRef();
-
+  const [apiData,setApiData]=useState([]);
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTIzYWQ1NGU2OTdjOTlhYWFkZjczMWMwODM2OGMyMyIsIm5iZiI6MTcyNjQ5NjQ4MC4wODQwMDEsInN1YiI6IjY0MTliY2I5MzEwMzI1MDA3YzBiNTI4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PxwASIUbQvY0xExPprdH3d78RHzWKjerTesgbie7gv4'
+    }
+  };
+  
+  
+    
   const handleWheel=(event)=>{
     event.preventDefault();
     cardsRef.current.scrollLeft +=event.deltaY;
   }
   useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err)); 
+
     cardsRef.current.addEventListener('wheel',handleWheel)
   },[])
   return (
     <div className='title_cards'>
       <h2>{title?title: "Popular on Netflix"}</h2>
       <div className="card_list" ref={cardsRef}>
-        {cards_data.map((card, index)=>{
+        {apiData.map((card, index)=>{
           return <div className="card" key={index}>
-              <img src={card.image} alt=""  />
-              <p>{card.name}</p>
+              <img src={`http://image.tmdb.org/t/p/w500`+card.backdrop_path} alt=""  />
+              <p>{card.original_title}</p>
           </div>
         })}
       </div>
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
-import './Player.css'
+import React, { useEffect, useState } from "react";
+import "./Player.css";
+import play_icon from "../../assets/play_icon.png";
 const Player = () => {
-  return (
-    <div>
-      Player
 
+  const [apiData, setApiData]=useState({
+    name:"",
+    key:"",
+    published_at:"",
+    typeof:''
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTIzYWQ1NGU2OTdjOTlhYWFkZjczMWMwODM2OGMyMyIsIm5iZiI6MTcyNjQ5NjQ4MC4wODQwMDEsInN1YiI6IjY0MTliY2I5MzEwMzI1MDA3YzBiNTI4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PxwASIUbQvY0xExPprdH3d78RHzWKjerTesgbie7gv4'
+    }
+  };
+  
+  useEffect(()=>{
+
+    fetch('https://api.themoviedb.org/3/movie/365177/videos?language=en-US', options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+  },[])
+
+
+
+  return (
+    <div className="player">
+      <img src={play_icon} alt="" />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.typeof}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default Player;
-
